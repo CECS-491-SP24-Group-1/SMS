14639a32b7498c842ec1b021aa1036727da3d338
jgilman1337 <85707243+jgilman1337@users.noreply.github.com>
2024-03-11 09:02:34 -0700

    Add docs to the MDB config struct

--- message_server/db/config.go
+++ message_server/db/config.go
@@ -8,11 +8,19 @@ page for more info on the available options.
 https://www.mongodb.com/docs/drivers/node/current/fundamentals/connection/connection-options/
 */
 type MConfig struct {
-	//THe l
-	Host     string `toml:"host" default:"127.0.0.1"`
-	Port     int    `toml:"port" default:"27017"`
-	AppName  string `toml:"app_name" default:"WraithAPI"`
+	//The address that the MDB server is located at.
+	Host string `toml:"host" default:"127.0.0.1"`
+
+	//The port that the MDB server is listening on.
+	Port int `toml:"port" default:"27017"`
+
+	//The name that this server should identify itself as when connecting to the database.
+	AppName string `toml:"app_name" default:"WraithAPI"`
+
+	//The username to connect to the database with.
 	Username string `toml:"username" default:""`
+
+	//The password to connect to the database with.
 	Password string `toml:"password" default:""`
 }
 
