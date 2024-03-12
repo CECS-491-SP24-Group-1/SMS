1e982520027cc278d65693eb5181de0ebdd517f8
jgilman1337 <85707243+jgilman1337@users.noreply.github.com>
2024-03-11 20:15:49 -0700

    Add tests constants and revise db structure

--- message_server/db/consts.go
+++ message_server/db/consts.go
@@ -1,9 +1,12 @@
 package db
 
 const (
-	//Denotes the root database that stores user objects.
-	USERS_DB = "users"
+	//Denotes the root database that stores all objects.
+	ROOT_DB = "wraith_db"
 
 	//Denotes the collection that stores user objects.
 	USERS_COLLECTION = "users"
+
+	//Denotes the collection that stores tests.
+	TESTS_COLLECTION = "tests"
 )
