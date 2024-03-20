d863971ed0aa601c39e0c474aac13340af1446b4
jgilman1337 <85707243+jgilman1337@users.noreply.github.com>
2024-03-19 19:57:04 -0700

    Add stringer for aggregation arrays

--- message_server/db/mongoutil/util.go
+++ message_server/db/mongoutil/util.go
@@ -26,6 +26,18 @@ func Aggregate(coll *mongo.Collection, filter bson.A, ctx context.Context) ([]bs
 	return results, nil
 }
 
+// Returns an array of matched documents from the database, given a string aggregation pipline.
+func AggregateS(coll *mongo.Collection, filter string, ctx context.Context) ([]bson.M, error) {
+	//Convert the string into a BSON aggregation array
+	agg, serr := String2Aggregation(filter)
+	if serr != nil {
+		return nil, serr
+	}
+
+	//Perform the aggregation
+	return Aggregate(coll, agg, ctx)
+}
+
 // Checks if a field exists in the database.
 func Exists(coll *mongo.Collection, filter bson.D, ctx context.Context) bool {
 	//Run the query on the database
@@ -35,4 +47,20 @@ func Exists(coll *mongo.Collection, filter bson.D, ctx context.Context) bool {
 	//Attempt to decode the document; this will throw an error if the query didn't return a document
 	//Thus, return true when no error occurs and false when there is one
 	return hit.Decode(&bson) == nil
+}
+
+/*
+Outputs a BSON array, representing a database aggregation pipeline, given
+a well-formed ExtJSON string. This function can be chained with `Aggregate()`
+to perform database queries from stringified Mongo aggregation piplines.
+This simplifies the way queries can be declared, allowing for convenient
+and simple edits. This function makes use of `bson.UnmarshalExtJSON()` to
+operate. See: https://pkg.go.dev/go.mongodb.org/mongo-driver@latest/bson#UnmarshalExtJSON
+*/
+func String2Aggregation(str string) (bson.A, error) {
+	var agg bson.A
+	if err := bson.UnmarshalExtJSON([]byte(str), false, &agg); err != nil {
+		return nil, err
+	}
+	return agg, nil
 }
