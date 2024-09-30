b5e045ae1588ab2eafa23cda5319a83227327ca0
Alfredomty <153565432+Alfredomty@users.noreply.github.com>
2024-09-30 14:51:04 -0700

    testing stuff, do not merge

--- message_server/task/purge_old_users_task.go
+++ message_server/task/purge_old_users_task.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 	"time"
+	"log"
 
 	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/bson"
+	"wraith.me/message_server/db"	// Accessing the db from message_server
 )
 
 // Purges old users from the database; implements `Task`.
@@ -15,6 +18,9 @@ type PurgeOldUsersTask struct {
 
 	//The Redis client to use in transactions.
 	RC *redis.Client
+	// The MongoDB client to use in transactions.
+	MongoClient *db.MClient
+
 
 	//The context to run the operations in.
 	CTX context.Context
@@ -36,4 +42,27 @@ func (ftt PurgeOldUsersTask) runOnStop() {
 
 func (ftt PurgeOldUsersTask) runPeriodically() {
 	fmt.Printf("fit tea task; run periodically; time: %s\n", time.Now().Format("15:04:05.000"))
+
+	// Check the database connection
+	if !ftt.MongoClient.IsConnected(){
+		log.Println("MongoDB client is not connected, skipping purge task")
+		return
+	}
+
+	// Accessing user collection
+	user_collection := ftt.MongoClient.GetClient()
+
+	// User flag filter
+	filter := bson.M{
+		"flags.should_purge": true,
+		"flags.purge_by": bson.M{"$lt": time.Now()},
+	}
+	// Perform the deletion
+	result, err := user_collection.RemoveAll(ftt.CTX, filter)
+	if err != nil {
+		log.Printf("Error purging users: %v", err)
+		return
+	}
+
+	log.Printf("Purged %d users from the database", result.DeletedCount)
 }
