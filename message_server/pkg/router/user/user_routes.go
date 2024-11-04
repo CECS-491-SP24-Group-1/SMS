27ef9c183c62beaf53ec8c19154fd848e7f9afe3
mtkatsaros <mtkatsaros@gmail.com>
2024-11-03 20:20:38 -0800

    TW-131 BE: "Change name" button functionality

---
+++ message_server/pkg/router/user/user_routes.go
@@ -0,0 +1,43 @@
+package user
+
+import (
+	"github.com/go-chi/chi/v5"
+	"wraith.me/message_server/pkg/config"
+	"wraith.me/message_server/pkg/globals"
+	"wraith.me/message_server/pkg/mw"
+	"wraith.me/message_server/pkg/schema/user"
+)
+
+var (
+	// Shared user collection across the entire package.
+	uc *user.UserCollection
+
+	// Shared config object across the entire package.
+	cfg *config.Config
+
+	// Shared env object across the entire package.
+	env *config.Env
+)
+
+// Sets up routes for the `/api/user` endpoint.
+func UserRoutes() chi.Router {
+	//Create the router
+	r := chi.NewRouter()
+
+	//Set the singletons for the entire package
+	uc = globals.UC
+	cfg = globals.Cfg
+	env = globals.Env
+
+	//Add routes (unauthenticated)
+	//None (for now)
+
+	//Add routes (authenticated)
+	r.Group(func(r chi.Router) {
+		r.Use(mw.NewAuthMiddleware(env))
+		r.Patch(`/changeName`, changeUserName)
+	})
+
+	//Return the router
+	return r
+}
